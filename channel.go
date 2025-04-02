911af7b4c350c3b26f93e38aaca51b6cd4c64688
Dmytro Steblyna <80773046+dimast-x@users.noreply.github.com>
2025-04-02 12:07:16 +0800

    feat: add signature methods, comments and initial readme (#1)
    * feat: add signature methods, comments and initial readme

--- channel.go
+++ channel.go
@@ -6,18 +6,17 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
-// Allocation represents a fund allocation in a channel
+// Allocation represents a fund allocation in a state channel.
 type Allocation struct {
-	Destination common.Address
-	Token       common.Address
-	Amount      *big.Int
+	Destination common.Address // Recipient of the funds
+	Token       common.Address // ERC-20 token address (zero address for ETH)
+	Amount      *big.Int       // Quantity of tokens allocated
 }
 
-// Channel represents the configuration of a state channel
+// Channel represents the configuration of a state channel between two participants.
 type Channel struct {
-	Participants [2]common.Address
-	Adjudicator  common.Address
-	Challenge    uint64
-	Nonce        uint64
+	Participants [2]common.Address // Addresses of the two channel parties
+	Adjudicator  common.Address    // Address of the dispute resolution contract
+	Challenge    uint64            // Challenge period duration in seconds
+	Nonce        uint64            // Unique identifier for this channel
 }
-
